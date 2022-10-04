73c2d2dcbe353e2ad2af8e1be36468bd024fa267
carlos <carmayuza@gmail.com>
2022-10-04 11:09:22 -0300

    creacion carpeta Backend
    Creacion e inclucion de archivos de spring boot initialzr

---
+++ Proyinteg2022/Backend/src/main/java/com/portfolio/crma/CrmaApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.crma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrmaApplication.class, args);
+	}
+
+}
